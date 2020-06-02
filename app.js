4ac8fce0f2d4c6547cac05843782b482efd8f48f
Valts Vilcans <valts03@inbox.lv>
2020-06-02 17:36:54 +0200

    [reworked as fetch]

--- app.js
+++ app.js
@@ -2,25 +2,36 @@ const fact = document.getElementById("fact");
 const factText = document.getElementById("factText");
 const input = document.getElementById("input");
 
-input.addEventListener("input", getFactAjax);
+input.addEventListener("input", getFactFetch);
 fact.addEventListener("click", closeFunc);
 
-function getFactAjax() {
+// function getFactAjax() {
+//   const xhr = new XMLHttpRequest();
+
+//   xhr.open("GET", `http://numbersapi.com/${number}`);
+
+//   xhr.onload = function () {
+//     if (this.status == 200 && number) {
+//       console.log(this.responseText);
+//       fact.classList.remove("d-none");
+//       factText.innerText = this.responseText;
+//     }
+//   };
+
+//   xhr.send();
+// }
+
+function getFactFetch() {
   const number = input.value;
-
-  const xhr = new XMLHttpRequest();
-
-  xhr.open("GET", `http://numbersapi.com/${number}`);
-
-  xhr.onload = function () {
-    if (this.status == 200 && number) {
-      console.log(this.responseText);
-      fact.classList.remove("d-none");
-      factText.innerText = this.responseText;
-    }
-  };
-
-  xhr.send();
+  fetch(`http://numbersapi.com/${number}`)
+    .then((response) => response.text())
+    .then((data) => {
+      if (number) {
+        fact.classList.remove("d-none");
+        factText.innerText = data;
+      }
+    })
+    .catch((err) => console.log("dangit"));
 }
 
 function closeFunc(e) {
